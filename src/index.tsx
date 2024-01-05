0e69df9603275cafcb31600dc08b195d89ee9cae
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-01-05 16:17:06 +0100

    create global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { GlobalStyle } from 'styles/Global.styled';
 
 ReactDOM.render(
-    <App />,
+  <>
+    <GlobalStyle/>
+    <App />
+  </>,
   document.getElementById('root')
 );
---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    line-height: 1.2;
+    min-width: 360px;
+  }
+
+  ul {
+    list-style: none;
+  }
+  
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
+  
+  button {
+    border: unset;
+    background-color: unset;
+  }
+  
+  p {
+    font-size: 18px;
+    font-weight: 400;
+  }
+`
