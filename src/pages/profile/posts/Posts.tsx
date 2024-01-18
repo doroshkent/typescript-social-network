e728c10e02374c4d6d216f1e0bc7e1cb9970a47c
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-01-18 18:09:34 +0100

    posts map refactor

--- src/pages/profile/posts/Posts.tsx
+++ src/pages/profile/posts/Posts.tsx
@@ -2,21 +2,34 @@ import React from 'react';
 import styled from "styled-components";
 import { Post } from './post/Post';
 import { AddNewPostForm } from "pages/profile/posts/addNewPostForm/AddNewPostForm";
+import { v4 } from "uuid";
+
+type PostType = {
+  id: string
+  content: string
+  likesCount: number
+}
+
+const posts: Array<PostType> = [
+  { id: v4(), content: "My first post", likesCount: 3 },
+  { id: v4(), content: "Need your support", likesCount: 2 },
+  { id: v4(), content: "Love React", likesCount: 10 },
+]
 
 export const Posts: React.FC = () => {
   return (
     <MyPosts>
       <h2>My Posts</h2>
       <AddNewPostForm />
-      <Post postContent={"My first post"} likesCount={3} />
-      <Post postContent={"Need your support"} likesCount={2} />
-      <Post postContent={"Love React"} likesCount={10} />
+      { posts.map( p => {
+        return <Post key={ p.id } postContent={ p.content } likesCount={ p.likesCount } />
+      } ) }
     </MyPosts>
   );
 };
 
 const MyPosts = styled.section`
- article + article {
-   margin-top: 10px;
- }
+  article + article {
+    margin-top: 10px;
+  }
 `
